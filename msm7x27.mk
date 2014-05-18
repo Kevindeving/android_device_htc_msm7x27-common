ceaa5e100f75e8b7bb2e43260c0e92f0e8ea5311
Kevin <98kevchen@gmail.com>
2014-05-18 12:21:14 +0200

    remove some languages not supported by smaller font footprint

--- msm7x27.mk
+++ msm7x27.mk
@@ -138,6 +138,12 @@ PRODUCT_LOCALES += mdpi
 # Fall back to hdpi when mdpi is missing
 PRODUCT_AAPT_CONFIG := normal mdpi hdpi
 PRODUCT_AAPT_PREF_CONFIG := mdpi
+PRODUCT_LOCALES := en_US en_IN fr_FR it_IT es_ES et_EE de_DE nl_NL cs_CZ \
+    pl_PL ja_JP zh_TW zh_CN zh_HK ru_RU ko_KR nb_NO es_US da_DK el_GR tr_TR \
+    pt_PT pt_BR rm_CH sv_SE bg_BG ca_ES en_GB fi_FI hr_HR hu_HU in_ID iw_IL \
+    lt_LT lv_LV ro_RO sk_SK sl_SI sr_RS uk_UA vi_VN tl_PH ar_EG fa_IR sw_TZ \
+    ms_MY af_ZA zu_ZA en_XA ar_XB fr_CA mn_MN hy_AM az_AZ ka_GE
+
 
 # Include Adreno200 vendor blobs
 # http://git.cryptomilk.org/projects/marvel/android_vendor_qcom_msm7x27.git/
